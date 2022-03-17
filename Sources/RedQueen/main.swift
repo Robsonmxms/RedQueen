feafd0b1302154af182f752a0b0de6a7940310f6
Robson Lima Lopes <robson060814@hotmail.com>
2022-03-17 10:17:54 -0300

    creating header

--- Sources/RedQueen/main.swift
+++ Sources/RedQueen/main.swift
@@ -1,12 +1,59 @@
 import Foundation
 
-var voiceRecognizedPhrase = "Open the lights"
+main()
 
-let voiceChosenAutomation = chosenAutomation[voiceRecognizedPhrase.lowercased()]
+func main(){
+    var isKeepLooping = true
 
-if let automationToRun = voiceChosenAutomation{
-    let automation = Automation()
-    automation.openAutomation(type: automationToRun)
+    while isKeepLooping{
+        buildingHeader()
+        isKeepLooping = keepChoosingAutomation()
+    }
 }
+
+func buildingHeader()
+{
+    print("""
+            \n\nGerenciador de automações
+
+    Escolha uma das opções abaixo:
+
+    [1] Abrir Site
+    [2] Acender as luzes
+    [3] Abrir refrigerador
+
+    Ou pressione qualquer outra tecla para encerrar
+    """)
+}
+
+func keepChoosingAutomation() -> Bool{
+    
+    var isKeepChoosingAutomation = false
+    
+    if let readInput = readLine()
+    {
+        if let chosenHeaderOption = Int(readInput)
+        {
+            isKeepChoosingAutomation = buildingAutomation(
+                chosenHeaderOption: chosenHeaderOption
+            )
+        }
+    }
+    return isKeepChoosingAutomation
+}
+
+func buildingAutomation(chosenHeaderOption : Int) -> Bool {
+    let chosenOptionAutomation = chosenAutomation[chosenHeaderOption]
+    var isbuilded = false
+    
+    if let automationToRun = chosenOptionAutomation{
+        let automation = Automation()
+        automation.openAutomation(type: automationToRun)
+        isbuilded = true
+    }
+    
+    return isbuilded
+}
+
 
 
