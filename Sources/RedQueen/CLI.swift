a1686ad5a21d4b187bc95b56d1a62a3f4335dbf0
Robson Lima Lopes <robson060814@hotmail.com>
2022-03-22 12:07:27 -0300

    new arquitecture

---
+++ Sources/RedQueen/CLI.swift
@@ -0,0 +1,64 @@
+import Foundation
+
+class CLI {
+    
+    func run(){
+        var isKeepLooping = true
+
+        while isKeepLooping{
+            buildingHeader()
+            isKeepLooping = isChoosingAutomation()
+        }
+    }
+    
+    func buildingHeader()
+    {
+        print("""
+                \n\nRed Queen
+
+        I control everything,
+        choose one of the automation options that I will run
+        
+        """)
+        
+        buildingOptions()
+
+        print("\nOr press any other key to end execution")
+        
+    }
+
+    func buildingOptions(){
+        for key in chosenAutomation.keys{
+            let value = chosenAutomation[key]
+            print("[",key,"]\t ",value!)
+        }
+    }
+
+    func isChoosingAutomation() -> Bool{
+        
+        let readInput = readLine()
+        
+        guard let chosenHeaderOption = Int(readInput!)else{
+            return false
+        }
+        
+        let wasChosen = isBuildingAutomation(
+            chosenHeaderOption: chosenHeaderOption
+        )
+            
+        return wasChosen
+    }
+
+    func isBuildingAutomation(chosenHeaderOption : Int) -> Bool {
+        let chosenOptionAutomation = chosenAutomation[chosenHeaderOption]
+        
+        if let automationToRun = chosenOptionAutomation{
+            let automation = Automation()
+            automation.openAutomation(type: automationToRun)
+            return true
+        }
+        
+        return false
+    }
+
+}
